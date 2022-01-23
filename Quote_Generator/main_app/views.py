9fc9e7664b96db9baafa67323f7573b3b8288b17
Michaudsr <michaudsr@gmail.com>
2022-01-22 23:45:41 -0500

    [templates] created a directory called templates in the main app and add hmtl file for about page, rendered in the main_app views

--- Quote_Generator/main_app/views.py
+++ Quote_Generator/main_app/views.py
@@ -7,4 +7,4 @@ def index(request):
     return HttpResponse('<h1>Hello World!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>I am Steve the developer</h1>')
+    return render(request, 'about.html')

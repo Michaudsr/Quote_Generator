643ea24dcec5df57ceb126699c2267db9dc01ac0
Michaudsr <michaudsr@gmail.com>
2022-01-24 01:24:14 -0500

    [about] update to text and icons

--- Quote_Generator/main_app/views.py
+++ Quote_Generator/main_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')

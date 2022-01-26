d18d73561e457ed9a06fc1e5d0d05f7c4227af69
Michaudsr <michaudsr@gmail.com>
2022-01-25 23:23:08 -0500

    [database] successfully connected to
    mysql

--- Quote_Generator/Quote_Generator/settings.py
+++ Quote_Generator/Quote_Generator/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL='/media/'
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'Quote_Generator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Quotegenerator',
+        'User': 'quotegeneratoruser',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- Quote_Generator/main_app/views.py
+++ Quote_Generator/main_app/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import os
 
 
-# Create your views here.
+
+################## Index ####################
 def index(request):
     return render(request, 'index.html')
 
+################## About ####################
 def about(request):
     return render(request, 'about.html')
+
+################## Signup ####################
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else: 
+            return HttpResponse('<h1>Try Again</h1>')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
